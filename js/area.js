999823731ce4709b1b40a88cd51045413a87658e
devsafi <mdkawserferdoussafi@gmail.com>
2024-02-12 21:47:42 +0600

    second commit

--- js/area.js
+++ js/area.js
@@ -17,11 +17,42 @@ function calculateRectangleArea() {
     console.log(area);
 }
 
+
 function calculateParallelogramArea() {
     const parallelogramBaseInput = document.getElementById('parallelogram-base');
     const base = parseFloat(parallelogramBaseInput.value);
     const parallelogramHeightInput = document.getElementById('parallelogram-height');
     const height = parseFloat(parallelogramHeightInput.value);
     const area = base * height;
+    console.log(area);
+}
+
+
+function calculateRhombusArea() {
+    const rhombusD1Input = document.getElementById('rhombus-d1');
+    const d1 = parseFloat(rhombusD1Input.value);
+    const rhombusD2Input = document.getElementById('rhombus-d2');
+    const d2 = parseFloat(rhombusD2Input.value);
+    const area = 0.5 * d1 * d2;
+    console.log(area);
+}
+
+
+function calculatePentagonArea() {
+    const pentagonPInput = document.getElementById('pentagon-p');
+    const pentagonP = parseFloat(pentagonPInput.value);
+    const pentagonBInput = document.getElementById('pentagon-b');
+    const pentagonB = parseFloat(pentagonBInput.value);
+    const area = 0.5 * pentagonP * pentagonB;
+    console.log(area);
+}
+
+
+function calculateEllipseArea() {
+    const ellipseAInput = document.getElementById('ellipse-a');
+    const ellipseA = parseFloat(ellipseAInput.value);
+    const ellipseBInput = document.getElementById('ellipse-b');
+    const ellipseB = parseFloat(ellipseBInput.value);
+    const area = 3.1416 * ellipseA * ellipseB;
     console.log(area);
 }
